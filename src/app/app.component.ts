6a46a0a865ab03b51b0d483104fbbf1771ce7621
Marina Kovel <m.kovel@softteco.com>
2024-12-30 18:52:33 +0300

    feat: add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButton } from '@angular/material/button'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButton],
+  imports: [CommonModule, RouterOutlet, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LoginComponent, RegistrationComponent } from "./components";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [{
+    path: 'login',
+    component: LoginComponent
+}, {
+    path: 'registration',
+    component: RegistrationComponent
+}];
 
 @NgModule({
     declarations: [LoginComponent, RegistrationComponent],
-    imports: [CommonModule]
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes)
+    ]
 })
 
 export class AuthModule {}
